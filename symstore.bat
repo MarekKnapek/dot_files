f2ae66fbd62477f93447c846550b57ae9eb004f3
Marek Knápek <knapek.mar@gmail.com>
2023-11-12 23:35:36 +0100

    symstore

---
+++ symstore.bat
@@ -0,0 +1,3 @@
+symstore add /r /f c:\dev\symbols\ /s c:\dev\mnt\mkdisk\dev\symbols\ /t product
+symstore add /r /f c:\dev\symbols\ /s c:\dev\mnt\mkdisk\dev\symbols_cab\ /t product /compress cab
+symstore add /r /f c:\dev\symbols\ /s c:\dev\mnt\mkdisk\dev\symbols_zip\ /t product /compress zip
