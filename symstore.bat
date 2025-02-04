864cb31fe98f185baa7ae5c5373c9713b2bcded5
Marek Knápek <knapek.mar@gmail.com>
2025-02-05 00:00:23 +0100

    update

--- symstore.bat
+++ symstore.bat
@@ -1,3 +1,4 @@
 symstore add /r /f c:\dev\symbols\ /s c:\dev\mnt\mkdisk\dev\symbols\ /t product
 symstore add /r /f c:\dev\symbols\ /s c:\dev\mnt\mkdisk\dev\symbols_cab\ /t product /compress cab
 symstore add /r /f c:\dev\symbols\ /s c:\dev\mnt\mkdisk\dev\symbols_zip\ /t product /compress zip
+"c:\Program Files (x86)\Windows Kits\8.1\Debuggers\x64\symstore.exe" add /r /f . /s c:\dev\mnt\mkdisk\dev\dev\symbols\ /t product
