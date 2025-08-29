7b5368803b68c62c2f405ac6ea61d317dc5427c1
Marek Knápek <knapek.mar@gmail.com>
2025-08-29 17:32:15 +0200

    hardlinks

--- hardlinks.bat
+++ hardlinks.bat
@@ -1,60 +1,75 @@
-@echo off
+@if "%~1"=="/inner" goto mk_inner
+@cmd.exe /c ""%~f0" /inner "%cd%" %*" || goto mk_fail
+@goto mk_end
 
-if "%~1"=="" goto mk_args_0
-if "%~2"=="" goto mk_args_1
-if "%~3"=="" goto mk_args_2
-if "%~4"=="" goto mk_args_3
-if "%~5"=="" goto mk_args_4
+:mk_inner
+@echo off
+if "%~3"=="" goto mk_args_0
+if "%~4"=="" goto mk_args_1
+if "%~5"=="" goto mk_args_2
+if "%~6"=="" goto mk_args_3
+if "%~7"=="" goto mk_args_4
 goto mk_args_5_or_more
+exit /b 1
 goto mk_end
 
 :mk_args_0
-echo "Call with two parameters, src and dst."
+echo Call with two parameters, src and dst.
 goto mk_end
 
 :mk_args_1
-echo "Call with two parameters, src and dst."
+echo Call with two parameters, src and dst. Error, 1 parameter provided.
+exit /b 1
 goto mk_end
 
 :mk_args_2
-if exist "%~f1\*" goto mk_args_2_dir
+if exist "%~f3\*" goto mk_args_2_dir
 goto mk_args_2_file
 :mk_args_2_dir
-pushd "%~f1"
-if %%errorlevel%%==1 goto mk_end
-for %%i in (*) do call "%~f0" "/cmd_file" "%~1" "%~2" "%%i"
-for /d %%i in (*) do call "%~f0" "/cmd_dir" "%~1\%%i" "%~2\%%i"
-popd
+pushd "%~f3" || goto mk_fail
+for %%i in (*) do call "%~f0" /inner %2 /cmd_file "%~3" "%~4" "%%i"
+for /d %%i in (*) do call "%~f0" /inner %2 /cmd_dir "%~3" "%~4" "%%i"
+popd || goto mk_fail
 goto mk_end
 :mk_args_2_file
-mklink /h "%~f2" "%~f1"
+pushd %2 || goto mk_fail
+mklink /h "%~4" "%~3" > nul 2>&1
+popd || goto mk_fail
 goto mk_end
 
 :mk_args_3
-if "%~1"=="/cmd_dir" goto mk_cmd_dir
-if "%~1"=="/cmd_create" goto mk_cmd_create
+echo Call with two parameters, src and dst. Error, 3 parameters provided.
+exit /b 1
 goto mk_end
 
 :mk_args_4
-if "%~1"=="/cmd_file" goto mk_cmd_file
+if "%~3"=="/cmd_file" goto mk_cmd_file
+if "%~3"=="/cmd_dir" goto mk_cmd_dir
+echo Bad sub-command.
+exit /b 1
 goto mk_end
 
 :mk_args_5_or_more
-echo "Call with two parameters, src and dst."
+echo Call with two parameters, src and dst. Error, too many parameters provided.
+exit /b 1
 goto mk_end
 
 :mk_cmd_file
-call "%~f0" "/cmd_create" "%~2\%~4" "%~3\%~4"
-goto mk_end
-
-:mk_cmd_create
-mkdir "%~dp3" 2> nul
-mklink /h "%~3" "%~2" > nul
+pushd "%~2" || goto mk_fail
+call "%~f0" /inner %2 "%~4\%~6" "%~5\%~6"
+popd || goto mk_fail
 goto mk_end
 
 :mk_cmd_dir
-rem Recursion, baby!
-call "%~f0" "%~2" "%~3"
+pushd "%~2" || goto mk_fail
+mkdir "%~5\%~6" 2>nul
+rem cmd.exe /c ""%~f0" /inner %2 "%~4\%~6" "%~5\%~6"" || goto mk_fail
+call "%~f0" /inner %2 "%~4\%~6" "%~5\%~6" || goto mk_fail
+popd || goto mk_fail
 goto mk_end
+
+:mk_fail
+@exit /b %errorlevel%
+@goto mk_end
 
 :mk_end
