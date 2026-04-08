f23fa488b71cf9482a4b5de334f1f492dd7237fe
Marek Knápek <knapek.mar@gmail.com>
2026-04-08 04:24:25 +0200

    git-clean

--- git-clean.bash
+++ git-clean.bash
@@ -10,44 +10,51 @@ fi
 mk_gh_name="${1}"
 mk_gh_repo="${2}"
 
-git clone --bare https://github.com/${mk_gh_name}/${mk_gh_repo}.git
+git init --bare ${mk_gh_repo}.git
 cd ${mk_gh_repo}.git
-mk_gh_branch="$(git rev-parse --abbrev-ref HEAD)"
-git worktree add ../${mk_gh_repo} ${mk_gh_branch} --relative-paths --no-checkout
+git remote add gh https://github.com/${mk_gh_name}/${mk_gh_repo}.git
 
+git config --local core.repositoryformatversion 1
+git config --local extensions.relativeWorktrees true
+git config --local gc.auto 0
 git config --local core.autocrlf false
 git config --local core.compression 9
 git config --local core.looseCompression 9
-git config --local extensions.relativeWorktrees true
-git config --local gc.auto 0
 git config --local pack.compression 9
-git config --local --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
-git config --local --add remote.origin.fetch +refs/tags/*:refs/tags/origin/*
-git config --local --unset-all remote.origin.fetch
-git config --local --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
-git config --local --add remote.origin.fetch +refs/tags/*:refs/tags/origin/*
-git config --local remote.origin.tagOpt --no-tags
+git config --local --add remote.gh.fetch +refs/heads/*:refs/remotes/gh/*
+git config --local --add remote.gh.fetch +refs/tags/*:refs/tags/gh/*
+git config --local --unset-all remote.gh.fetch
+git config --local --add remote.gh.fetch +refs/heads/*:refs/remotes/gh/*
+git config --local --add remote.gh.fetch +refs/tags/*:refs/tags/gh/*
+git config --local remote.gh.tagOpt --no-tags
 
+git fetch --all --force --prune
+
+mk_gh_remote_branch="$(git symbolic-ref --short refs/remotes/gh/HEAD)"
+mk_gh_branch="${mk_gh_remote_branch#*/}"
+git worktree add --orphan -b ${mk_gh_branch} ../STL --relative-paths
+
+cd ../${mk_gh_repo}
+git reset --hard gh/${mk_gh_branch}
+git branch --set-upstream-to=gh/${mk_gh_branch} ${mk_gh_branch}
+git fetch --all --force --prune
+git pull
+
+cd ../${mk_gh_repo}.git
 rm -rf hooks/
 rm -rf info/exclude
 rm -rf description
 rm -rf packed-refs
 rm -rf refs/tags/
 
-cd ../${mk_gh_repo}
-git fetch --all --force --prune
-git pull origin ${mk_gh_branch}
-git branch --set-upstream-to=origin/${mk_gh_branch} ${mk_gh_branch}
-
-cd ../${mk_gh_repo}.git
 rm -rf logs/
 rm -rf worktrees/${mk_gh_repo}/logs/
-rm -rf worktrees/${mk_gh_repo}/FETCH_HEAD
-rm -rf worktrees/${mk_gh_repo}/index
 
 cd ../${mk_gh_repo}
 git clean -dfx
 git reset --hard --
+git fetch --all --force --prune
+git pull
 
 cd ../${mk_gh_repo}.git
 rm -rf logs/
